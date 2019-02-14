ee333043d649213f59a6cd4b5f4391db59d2dd1c
Joshisaka1004 <joshi1004@gmail.com>
2019-02-14 18:22:04 +0100

    First Setup with MenueVC and DetailVC

---
+++ CoreData_First_Realm_Second/DetailVC.swift
@@ -0,0 +1,20 @@
+//
+//  DetailVC.swift
+//  CoreData_First_Realm_Second
+//
+//  Created by Joachim Vetter on 14.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func addButton(_ sender: UIBarButtonItem) {
+    }
+}
