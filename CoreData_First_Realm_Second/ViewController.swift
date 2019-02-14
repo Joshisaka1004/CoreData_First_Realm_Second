558a92380e382551f77fffda62fdbc480fb478d3
Joshisaka1004 <joshi1004@gmail.com>
2019-02-14 17:59:52 +0100

    Initial Commit

---
+++ CoreData_First_Realm_Second/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData_First_Realm_Second
+//
+//  Created by Joachim Vetter on 14.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
