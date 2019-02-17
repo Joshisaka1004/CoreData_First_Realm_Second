b73ab4ce935141c9a99cd23b8008f9bd34f17a2c
Joshisaka1004 <joshi1004@gmail.com>
2019-02-17 23:20:49 +0100

    Trying to replace some stuff from Core Data to realm by introducing two new classes instead of the entities

---
+++ CoreData_First_Realm_Second/Menue.swift
@@ -0,0 +1,14 @@
+//
+//  Menue.swift
+//  CoreData_First_Realm_Second
+//
+//  Created by Joachim Vetter on 17.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Menue: Object {
+    @objc dynamic var items: String = ""
+}
---
+++ CoreData_First_Realm_Second/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  CoreData_First_Realm_Second
+//
+//  Created by Joachim Vetter on 17.02.19.
+//  Copyright © 2019 Joachim Vetter. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Task: Object {
+    @objc dynamic var name: String = ""
+}
